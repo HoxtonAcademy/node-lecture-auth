c7493426054855167bac5c617a511b2e62c9e53a
Ed 'Quacker' Putans <edputans@gmail.com>
2021-12-08 13:50:18 +0000

    updated to match slides

--- index.ts
+++ index.ts
@@ -1,64 +1,53 @@
+import bcrypt from 'bcryptjs';
+import randomstring from 'randomstring';
+
+import jwt from 'jsonwebtoken';
+import dotEnv from 'dotenv';
+
+dotEnv.config()
+
 import express from 'express';
 import cors from 'cors';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
 import Database from 'better-sqlite3';
 
-require('dotenv').config()
-
 const db = new Database(
-    'nukes.db',
+    'nukers.db',
     { verbose: console.log }
 );
 
-const findUserByUsername = db.prepare(`SELECT * FROM users WHERE username=?;`)
-const findUserById = db.prepare(`SELECT * FROM users WHERE id=?;`)
-const writeQuery = db.prepare(`INSERT INTO users (username, password) VALUES (?, ?);`)
+const getNuclearCode = () => randomstring.generate();
+
+const init = db.prepare(`CREATE TABLE IF NOT EXISTS users (
+    username  TEXT NOT NULL UNIQUE,
+    password  TEXT NOT NULL,
+    id        INTEGER PRIMARY KEY AUTOINCREMENT
+);`)
+
+init.run();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const frenchNukeCodes: string[] = ['12345', 'abcde', 'hello world', 'juan'];
+const findUserByUsername = db.prepare(`SELECT * FROM users WHERE username=?;`)
+const findUserById = db.prepare(`SELECT * FROM users WHERE id=?;`);
+const createUser = db.prepare(`INSERT INTO users (username, password) VALUES (?, ?);`);
 
-app.get('/codes', (req, res) => {
-    const { authorization } = req.headers;
-
-    if (authorization) {
-        const decodedUserObject = jwt.verify(authorization, process.env.JWT_TOKEN);
-
-        console.log(decodedUserObject);
-
-        if (decodedUserObject) {
-            res.json({ codes: frenchNukeCodes });
-        } else {
-            res.status(400).json({ error: "Something went wrong, log in again sil vous ples " })
-        }
-    }
-});
-
-app.post('/register', async (req, res) => {
+app.post('/register', (req, res) => {
     try {
         const { username, password } = req.body;
 
-        if (!(username && password)) {
-            return res.status(400).json({ error: "All inputs are required" });
-        }
+        if (!(username && password)) return res.status(400).json({ error: "All inputs are required" });
 
-        const item = findUserByUsername.get(username);
-        if (item) return res.status(400).json({ error: 'Le user exists already!!!' })
+        const existingUser = findUserByUsername.get(username);
+        if (existingUser) return res.status(400).json({ error: 'Le user exists already!!!' });
 
-        const encryptedPassword = await bcrypt.hash(password, 10);
-        const { lastInsertRowid } = writeQuery.run(username, encryptedPassword);
+        const encryptedPassword = bcrypt.hashSync(password, 10);
+        createUser.run(username, encryptedPassword);
 
-        // console.log(password);
-        // console.log(password, ` => `, encryptedPassword, lastInsertRowid);
-
-        res.json({ message: `registered ${username} hon hon hon with id ${lastInsertRowid} 🍇` })
-        // token: jwt.sign({ user_id: lastInsertRowid }, process.env.JWT_TOKEN)
-        // });
+        res.json({ message: `registered ${username} hon hon hon 🍇 vous can log in now` })
     } catch (error) {
-        res.status(400).json({ error })
+        res.status(400).json({ error: "Something went wrong ;/" })
     }
 })
 
@@ -66,18 +55,34 @@ app.post('/login', async (req, res) => {
     try {
         const { username, password } = req.body;
         const user = findUserByUsername.get(username);
-        if (!username || !user || !password) return res.status(400).json({ error: "Missing or incorrect details" });
+        if (!user) return res.status(400).json({ error: "Missing or incorrect details" });
 
-        const isPasswordCorect = bcrypt.compare(password, user?.password);
-        if (!isPasswordCorect) return res.status(400).json({ error: "Incorrect details" });
+        const isPasswordCorrect = bcrypt.compareSync(password, user.password);
+        if (!isPasswordCorrect) return res.status(400).json({ error: "Missing or incorrect details" });
 
         const token = jwt.sign({ user_id: user.id }, process.env.JWT_TOKEN);
-        res.json({ token, codes: frenchNukeCodes });
+
+        return res.json({ token, code: getNuclearCode() });
 
     } catch (error) {
-        res.status(400).json({ error })
+        res.status(400).json({ error: "Something went wrong ;/" })
     }
 })
+
+app.get('/code', (req, res) => {
+    if (!req.headers.authorization) return res.status(400).json({ error: "log in sil te plait" })
+
+    const { authorization } = req.headers;
+
+    const decodedUserObject = jwt.verify(authorization, process.env.JWT_TOKEN);
+    if (!decodedUserObject) return res.status(400).json({ error: "Something went wrong, log in again sil vous ples " })
+
+    const user = findUserById.get(decodedUserObject.user_id);
+    if (!user) return res.status(400).json({ error: "Something went wrong, log in again sil vous ples " })
+
+    res.json({ code: getNuclearCode() });
+});
+
 
 app.listen(3000, () => {
     console.info('listening on port 3000');
